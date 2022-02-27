f425632802334b7fc9513e6a96e08c4cfc765383
Atul <atulkharwal9329@gmail.com>
2022-02-27 17:02:25 +0530

    logout

--- AppUi/src/context/AuthActions.js
+++ AppUi/src/context/AuthActions.js
@@ -4,6 +4,10 @@ export const LoginStart = (userCredentials) => ({
   type: "LOGIN_START",
 });
 
+export const Logout = () => ({
+  type: "LOGOUT",
+});
+
 export const LoginSuccess = (user) => ({
   type: "LOGIN_SUCCESS",
   payload: user,
---
+++ AppUi/src/postContext/AuthActions.js
@@ -0,0 +1,8 @@
+/** @format */
+
+export const LoadPost = () => ({
+  type: "RELOAD",
+});
+export const StopReloadPost = () => ({
+  type: "STOPRELOAD",
+});
